fd643361a0ecbb48d25ba9be10280824bb06571f
S13Tyler <infinityfabrications@yahoo.com>
2024-09-15 04:52:02 -0700

    add basic screen render

--- main.py
+++ main.py
@@ -1,10 +1,39 @@
 #
-# open-source pygame library
+#
+#
+
 import pygame
+from constants import *
 
 
 def main():
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+
+    while running:
+        # pygame.QUIT event means the user clicked X to close your window
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # fill the screen with a color to wipe away anything from last frame
+        screen.fill("gray")
+
+        # RENDER YOUR GAME HERE
+
+        # flip() the display to put your work on screen
+        pygame.display.flip()
+
+        # limits FPS to 60
+        clock.tick(60)
+
+    pygame.quit()
 
 
 if __name__ == "__main__":
