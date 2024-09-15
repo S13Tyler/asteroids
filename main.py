7eea615814cdce0517f13b7440cff387b469bd70
S13Tyler <infinityfabrications@yahoo.com>
2024-09-15 05:35:47 -0700

    add player class

--- main.py
+++ main.py
@@ -3,7 +3,9 @@
 #
 
 import pygame
+from pygame import Vector2
 from constants import *
+from player import *
 
 
 def main():
@@ -11,11 +13,18 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
 
+    # Setup pygame display
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    running = True
+    dt = 0 # delta-time
 
+    # Create player object
+    spawn_pos = Vector2(screen.get_width() / 2, screen.get_height() / 2)
+    player = Player(spawn_pos.x, spawn_pos.y)
+
+    # Game loop
+    running = True
     while running:
         # pygame.QUIT event means the user clicked X to close your window
         for event in pygame.event.get():
@@ -23,15 +32,16 @@ def main():
                 running = False
 
         # fill the screen with a color to wipe away anything from last frame
-        screen.fill("gray")
+        screen.fill("black")
 
         # RENDER YOUR GAME HERE
+        player.draw(screen)
 
         # flip() the display to put your work on screen
         pygame.display.flip()
 
         # limits FPS to 60
-        clock.tick(60)
+        dt = clock.tick(60) / 1000
 
     pygame.quit()
 
